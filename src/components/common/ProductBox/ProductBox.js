c8f9ec4ab9887075d186d1e54284f842a59de8fd
RezaiMartyna <73740511+RezaiMartyna@users.noreply.github.com>
2021-04-14 20:59:21 +0200

    WDP210402-6

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,7 @@ import {
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 
-const ProductBox = ({ name, price, promo, stars }) => (
+const ProductBox = ({ name, price, promo, stars, oldPrice }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -51,6 +51,13 @@ const ProductBox = ({ name, price, promo, stars }) => (
           $ {price}
         </Button>
       </div>
+      {oldPrice && (
+        <div className={styles.oldprice}>
+          <Button noHover variant='small' className={styles.buttonPrice}>
+            $ {oldPrice}
+          </Button>
+        </div>
+      )}
     </div>
   </div>
 );
@@ -59,6 +66,7 @@ ProductBox.propTypes = {
   children: PropTypes.node,
   name: PropTypes.string,
   price: PropTypes.number,
+  oldPrice: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
 };
