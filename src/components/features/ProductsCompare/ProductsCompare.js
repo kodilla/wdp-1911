f4d4d50fe422134c2f0e4218629c83252c8837b6
stiffelbo <partygrouppl@gmail.com>
2021-04-19 14:49:52 +0200

    add compare button

--- src/components/features/ProductsCompare/ProductsCompare.js
+++ src/components/features/ProductsCompare/ProductsCompare.js
@@ -2,30 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ProductsCompare.module.scss';
 import Button from '../../common/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 import ProductToCompare from '../../common/ProductToCompare/ProductToCompare';
 
 class ProductsCompare extends React.Component {
   render() {
     const { compareProducts, addCompare } = this.props;
-    return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className='row'>
-            {compareProducts &&
-              compareProducts.map(item => (
+    if (compareProducts && compareProducts.length) {
+      return (
+        <div className={styles.root}>
+          <div className='container'>
+            <div className={`row ${styles.compareRow}`}>
+              {compareProducts.map(item => (
                 <div key={item.id} className={`col-2 ${styles.ProductToCompare}`}>
                   <ProductToCompare {...item} addCompare={addCompare} />
                 </div>
               ))}
-            {() => {
-              if (compareProducts.length) {
-                return <Button>Compare</Button>;
-              }
-            }}
+              <div key={'compareBtn'} className={`col-2 ${styles.ProductToCompare}`}>
+                <Button title={'compare products'}>
+                  <FontAwesomeIcon
+                    icon={faExchangeAlt}
+                    className={styles.icon}
+                  ></FontAwesomeIcon>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
