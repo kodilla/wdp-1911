4080a322834f3967354a531b1a43199386416dd0
Marcin Pytlak <marcin.pytlak@o2.pl>
2021-04-20 18:35:59 +0200

    test commit tuesday

--- src/components/common/StarRating/StarRating.js
+++ src/components/common/StarRating/StarRating.js
@@ -3,105 +3,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import styles from './StarRating.modules.scss';
 import PropTypes from 'prop-types';
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
 const addMyRatingToState = (id, addRating, value, myRating, event) => {
   event.preventDefault();
   addRating({ id: id, myRating: value });
 };
 
-const addHoverToState = (id, addHover, value, myHover, event) => {
-  event.preventDefault();
-  addHover({ id: id, myHover: value });
+const colorSet = (rating, value, stars) => {
+  if ((rating && rating < value) || (!rating && stars < value)) {
+    return '#F2F2F2';
+  } else if (!rating && stars < value) {
+    return '#000000';
+  } else if (rating & (rating >= value)) {
+    return '#e6e29c';
+  }
 };
-
 const starsArray = [1, 2, 3, 4, 5];
 
-const StarRating = ({ myRating, stars, addRating, id, addHover, myHover }) =>
-  myRating && myRating != null ? (
-    <div className={styles.starRatio}>
-      {starsArray.map(star => (
-        <label key={star} className={styles.starIcon}>
-          <input
-            type='radio'
-            name='rating'
-            value={starsArray.indexOf(star) + 1}
-            onClick={event =>
-              addMyRatingToState(
-                id,
-                addRating,
-                starsArray.indexOf(star) + 1,
-                myRating,
-                event
-              )
-            }
-          />
-          <FontAwesomeIcon
-            icon={faStar}
-            className={styles.hoverEfect}
-            onMouseEnter={event =>
-              addHoverToState(
-                id,
-                addHover,
-                starsArray.indexOf(star) + 1,
-                myHover,
-                event
-              )
-            }
-            onMouseLeave={event => addHoverToState(id, addHover, 0, myHover, event)}
-            color={
-              (myHover || myRating) < starsArray.indexOf(star) + 1 ? 'grey' : 'gold'
-            }
-          ></FontAwesomeIcon>
-        </label>
-      ))}
-    </div>
-  ) : (
-    <div>
-      {[1, 2, 3, 4, 5].map(i => (
-        <a key={i} href='#'>
-          {i <= stars ? (
-            <FontAwesomeIcon
-              icon={faStar}
-              onClick={event =>
-                addMyRatingToState(
-                  id,
-                  addRating,
-                  starsArray.indexOf(i) + 1,
-                  myRating,
-                  event
-                )
-              }
-            >
-              {i} stars
-            </FontAwesomeIcon>
-          ) : (
-            <FontAwesomeIcon
-              icon={farStar}
-              onClick={event =>
-                addMyRatingToState(
-                  id,
-                  addRating,
-                  starsArray.indexOf(i) + 1,
-                  myRating,
-                  event
-                )
-              }
-            >
-              {i} stars
-            </FontAwesomeIcon>
-          )}
-        </a>
-      ))}
-    </div>
-  );
+const StarRating = ({ myRating, stars, addRating, id }) => (
+  <div className={styles.starRatio}>
+    {starsArray.map(star => (
+      <label key={star} className={styles.starIcon}>
+        <input
+          type='radio'
+          name='rating'
+          value={starsArray.indexOf(star) + 1}
+          onClick={event =>
+            addMyRatingToState(
+              id,
+              addRating,
+              starsArray.indexOf(star) + 1,
+              myRating,
+              event
+            )
+          }
+        />
+        <FontAwesomeIcon
+          icon={faStar}
+          style={{ color: colorSet(myRating, starsArray.indexOf(star) + 1, stars) }}
+        ></FontAwesomeIcon>
+      </label>
+    ))}
+  </div>
+);
 StarRating.propTypes = {
   myRating: PropTypes.any,
   stars: PropTypes.number,
   addRating: PropTypes.func,
   id: PropTypes.string,
-  addHover: PropTypes.func,
-  myHover: PropTypes.any,
 };
 
 export default StarRating;
