7d3fdfead15c6a4fe96259917e51e5196795d43f
Marcin Pytlak <marcin.pytlak@o2.pl>
2021-04-20 20:59:33 +0200

    test commit with solution

--- src/components/common/StarRating/StarRating.js
+++ src/components/common/StarRating/StarRating.js
@@ -9,48 +9,53 @@ const addMyRatingToState = (id, addRating, value, myRating, event) => {
   addRating({ id: id, myRating: value });
 };
 
-const colorSet = (rating, value, stars) => {
-  if ((rating && rating < value) || (!rating && stars < value)) {
-    return '#F2F2F2';
-  } else if (!rating && stars < value) {
-    return '#000000';
-  } else if (rating & (rating >= value)) {
+const colorSet = (rating, value, hoverRating, stars) => {
+  if (hoverRating >= value) {
     return '#e6e29c';
+  } else if (!hoverRating && rating >= value) {
+    return '#e6e29c';
+  } else if (!rating && stars >= value) {
+    return '#000000';
   }
+  return '#F2F2F2';
 };
+
 const starsArray = [1, 2, 3, 4, 5];
 
-const StarRating = ({ myRating, stars, addRating, id }) => (
-  <div className={styles.starRatio}>
-    {starsArray.map(star => (
-      <label key={star} className={styles.starIcon}>
-        <input
-          type='radio'
-          name='rating'
-          value={starsArray.indexOf(star) + 1}
-          onClick={event =>
-            addMyRatingToState(
-              id,
-              addRating,
-              starsArray.indexOf(star) + 1,
-              myRating,
-              event
-            )
-          }
-        />
-        <FontAwesomeIcon
-          icon={faStar}
-          style={{ color: colorSet(myRating, starsArray.indexOf(star) + 1, stars) }}
-        ></FontAwesomeIcon>
-      </label>
-    ))}
-  </div>
-);
+const StarRating = ({ myRating, stars, addRating, id }) => {
+  const [hoverRating, setHoverRating] = React.useState(0);
+  const setHover = star => {
+    setHoverRating(star);
+  };
+  return (
+    <div className={styles.starRatio}>
+      {starsArray.map(star => (
+        <label key={star} className={styles.starIcon}>
+          <input
+            type='radio'
+            name='rating'
+            value={star}
+            onClick={event => addMyRatingToState(id, addRating, star, myRating, event)}
+          />
+          <FontAwesomeIcon
+            icon={faStar}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            //className={colorSet(myRating, star, hoverRating)}
+            style={{ color: colorSet(myRating, star, hoverRating, stars) }}
+          ></FontAwesomeIcon>
+        </label>
+      ))}
+    </div>
+  );
+};
 StarRating.propTypes = {
   myRating: PropTypes.any,
   stars: PropTypes.number,
   addRating: PropTypes.func,
   id: PropTypes.string,
+  setHover: PropTypes.func,
+  hoverRating: PropTypes.number,
 };
 
 export default StarRating;
