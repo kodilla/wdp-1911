c3b138dc7d3f7c359f6105f2dafa4fd6a7a0b0c7
RezaiMartyna <73740511+RezaiMartyna@users.noreply.github.com>
2021-04-25 21:46:26 +0200

    homepage marge

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -2,7 +2,6 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import styles from './Homepage.module.scss';
-
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Feedback from '../../layout/Feedback/FeedbackContainer';
