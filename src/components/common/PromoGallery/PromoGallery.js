34ac047e5a70282ac991c5e2d95d31768ac92692
Marcin Starzynski <gienekmsms@gmail.com>
2021-04-26 16:30:41 +0200

    Fix promoBox component

--- src/components/common/PromoGallery/PromoGallery.js
+++ src/components/common/PromoGallery/PromoGallery.js
@@ -2,14 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './PromoGallery.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
 
 const PromoGallery = ({ photoBackground, name, id }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       <img src={photoBackground} alt={name} />
+    </div>
+    <div className={styles.photoDescription}>
+      <p>
+        Indoor <span>furniture</span>
+      </p>
+      <p>save up to 50% of all furniture</p>
+      <Button variant='small' className={styles.button}>
+        shop now
+      </Button>
     </div>
   </div>
 );
